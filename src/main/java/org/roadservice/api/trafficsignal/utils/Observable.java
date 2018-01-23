4ae35d2f0c1423ef3523d03c9e9510f8b9942a2e
ajay upadhyay <ajay.upy@gmail.com>
2018-01-23 05:44:41 -0600

    Removed old code.

--- src/main/java/org/roadservice/api/trafficsignal/utils/Observable.java
+++
@@ -1,7 +0,0 @@
-package org.roadservice.api.trafficsignal.utils;
-
-public interface Observable {
-    public void addObserver(Observer observer);
-    public void removeObserver(Observer observer);
-    public void notifyObserver();
-}
--- src/main/java/org/roadservice/api/trafficsignal/utils/Observer.java
+++
@@ -1,5 +0,0 @@
-package org.roadservice.api.trafficsignal.utils;
-
-public interface Observer {
-    public void update(int signalStatus, boolean redToGreen,long signalID);
-}
